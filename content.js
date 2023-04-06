a8e9c49170f0ac7f3322416ab838b4fb647b7e14
Petr Savichev (proton) <psavichev@gmail.com>
2023-04-05 18:30:24 -0600

    filter by minimum reviews count

--- content.js
+++ content.js
@@ -18,10 +18,38 @@ const mySection = `
 const amazonFiltersTag = document.querySelector('#s-refinements > .a-section')
 amazonFiltersTag.innerHTML = mySection + amazonFiltersTag.innerHTML
 
-const amazonFilters = {}
+const amazonFilterTags = {}
 amazonFilterTags.minimumReviewsCount = document.getElementById('custom-amazon-filter-minimal-reviews-count')
 amazonFilterTags.negativeWords       = document.getElementById('custom-amazon-filter-negative-words')
 amazonFilterTags.minPrice            = document.getElementById('low-price')
-amazonFilterTags.minPrice            = document.getElementById('high-price')
+amazonFilterTags.maxPrice            = document.getElementById('high-price')
 
-//
+const elementToggle = (element, show) => {
+  element.style.display = show ? 'block' : 'none'
+}
+
+const filterProducts = _ => {
+  const amazonFilters = {}
+  amazonFilters.minimumReviewsCount = +amazonFilterTags.minimumReviewsCount.value
+  amazonFilters.negativeWords       = amazonFilterTags.negativeWords.value.split(/,|\n/).map(word => word.trim())
+  amazonFilters.minPrice            = +amazonFilterTags.minPrice.value
+  amazonFilters.maxPrice            = +amazonFilterTags.maxPrice.value
+
+  const products = document.querySelectorAll('.s-search-results [data-component-type="s-search-result"]')
+  for (const product of products) {
+    const reviewsCount = +product.querySelector('.a-size-small a .a-size-base').innerText.match(/(\d+)/)[0]
+    // const reviewsCount = product.querySelector('.a-size-small .a-size-base').innerText
+    // const negativeWords = amazonFilterTags.negativeWords.value.split(',')
+    // const negativeWordsFound = negativeWords.filter(word => product.innerText.includes(word)).length > 0
+    // const minPrice = amazonFilterTags.minPrice.value
+    // const maxPrice = amazonFilterTags.maxPrice.value
+    // const price = product.querySelector('.a-price-whole').innerText
+    // const show = reviewsCount >= amazonFilterTags.minimumReviewsCount.value && !negativeWordsFound && price >= minPrice && price <= maxPrice
+    let show = (reviewsCount >= amazonFilters.minimumReviewsCount) && true
+    elementToggle(product, show)
+  }
+}
+
+for (const key in amazonFilterTags) {
+  amazonFilterTags[key].addEventListener('change', filterProducts)
+}
