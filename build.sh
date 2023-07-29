7f30f304f02a12fddfe2f7418f0c506417b097d9
Petr Savichev (proton) <psavichev@gmail.com>
2023-07-29 17:51:19 -0500

    update manifest

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-zip -r -FS better-amazon.zip * --exclude '*.git*' --exclude '*.zip' --exclude '*/.DS_Store' .
+zip -r -FS better-amazon.zip * --exclude '*.git*' --exclude '*.zip' --exclude '*/.DS_Store' --exclude 'screenshots/*' .
