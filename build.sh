54842e529dc4156c2b07bbb09f7aa70c96340c1c
Petr Savichev (proton) <psavichev@gmail.com>
2025-09-06 09:42:02 -0500

    exclude .sh from the build

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-zip -r -FS better-amazon.zip * --exclude '*.git*' --exclude '*.zip' --exclude '*/.DS_Store' --exclude 'screenshots/*' .
+zip -r -FS better-amazon.zip * --exclude '*.git*' --exclude '*.zip' --exclude '*/.DS_Store' --exclude 'screenshots/*' --exclude '*.sh' .
