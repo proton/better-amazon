974dd606594260be855c46caa77fb745b2d61803
Petr Savichev (proton) <psavichev@gmail.com>
2025-09-07 18:41:56 -0500

    fix logic

--- background.js
+++ background.js
@@ -1,20 +1,28 @@
 const getIconsPath = (isGrayed) => {
   const iconSizes = [48, 128, 512]
   const colorSuffix = isGrayed ? 'grey-' : ''
-
   return iconSizes.reduce((acc, size) => {
     acc[size] = `images/icon-${colorSuffix}${size}.png`
     return acc
   }, {})
 }
 
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  if (changeInfo.status === 'complete' && tab.url) {
-    const isAmazon = tab.url.includes('amazon.')
+function updateIconForTab(tabId, url) {
+  const isAmazon = url?.includes('amazon.')
+  chrome.action.setIcon({
+    path: getIconsPath(!isAmazon),
+    tabId,
+  })
+}
 
-    chrome.action.setIcon({
-      path: getIconsPath(!isAmazon),
-      tabId,
-    })
+chrome.tabs.onUpdated.addListener((tabId, _changeInfo, tab) => {
+  if (tab.url) {
+    updateIconForTab(tabId, tab?.url)
   }
+})
+
+chrome.tabs.onActivated.addListener(activeInfo => {
+  chrome.tabs.get(activeInfo.tabId, (tab) => {
+    updateIconForTab(tab.id, tab?.url)
+  })
 })
